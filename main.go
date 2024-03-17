43cd508b6634692e41f79911660cf9b328a6d717
Conor Gallagher <dontlisten33@gmail.com>
2024-03-16 20:15:44 -0700

    subgraph and rpc update for perseus-sepolia

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	var netCfgPath = flag.String("netCfg", "./config/networks.json", "network config file")
+	var netCfgPath = flag.String("netCfg", "./config/testnets.json", "network config file")
 	var apiPath = flag.String("apiPath", "gcgo", "API server root path")
 	var noRpcMode = flag.Bool("noRpcMode", false, "Run in mode with no RPC calls")
 	flag.Parse()
